6d0a84fa3659ff68154020e587ca0aa5bb7045ad
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-10-15 17:19:15 +0530

    Create campare.cpp

---
+++ campare.cpp
@@ -0,0 +1,33 @@
+    int countPrimes(int n) {
+    if(n<=2)
+    return 0;
+    const vector<bool> isPrime = sieveEratosthenes(n);
+    return count(isPrime.begin(), isPrime.end(), true);
+    }
+    private:
+    vector<bool> sieveEratosthenes(int n){
+        vector<bool> isPrime(n, true);
+        isPrime[0]=false;
+        isPrime[1]=false;
+        for(int i=2; i*i<n; i++)
+        if(isPrime[i])
+        for(int j=i*i; j<n; j+=i)
+        isPrime[j]=false;
+        return isPrime;
+-----------------------------------------------------------------
+  int countPrimes(int n) {
+    if (n <= 2)
+      return 0;
+    const vector<bool> isPrime = sieveEratosthenes(n);
+    return count(isPrime.begin(), isPrime.end(), true);
+  }
+ private:
+  vector<bool> sieveEratosthenes(int n) {
+    vector<bool> isPrime(n, true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for (int i = 2; i * i < n; ++i)
+      if (isPrime[i])
+        for (int j = i * i; j < n; j += i)
+          isPrime[j] = false;
+    return isPrime;
