695d80bfb07ae59a3f2bc257d3a739b945a1f93b
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-10-21 21:08:12 +0530

    Update campare.cpp

--- campare.cpp
+++ campare.cpp
@@ -31,3 +31,50 @@
         for (int j = i * i; j < n; j += i)
           isPrime[j] = false;
     return isPrime;
+------------------------------------------------------------------------------------------
+    #include<bits/stdc++.h>
+using namespace std; 
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        for(int k=2; k<=1e8; k++){
+            int p=pow(2, k);
+            --p;
+            if(n%p==0){
+                cout<<n/p<<endl;
+                break;
+            }}}
+return 0;
+}
+
+ #include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n; // You need to read the value of n for each test case.
+        cin >> n;
+        for (int k = 2; k <= 1e8; k++) {
+            int p = pow(2, k);
+            --p;
+            if (n % p == 0) {
+                cout << n / p << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+
